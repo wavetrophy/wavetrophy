38d10bf7cad6bddb6a2e27f774b57ef74e83f388
= <bjoern.pfoster@gmail.com>
2019-03-06 22:15:31 +0100

    Fixed deployment

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -39,6 +39,7 @@ return new class extends DefaultDeployer
     {
         // see https://github.com/EasyCorp/easy-deploy-bundle/issues/35
         // see https://github.com/EasyCorp/easy-deploy-bundle/blob/14edd418d82d2c616d79ecf2830c6140b0dc3971/src/Deployer/DefaultDeployer.php
+        $this->runRemote('cp {{ deploy_dir }}/repo/.env {{ project_dir }} 2>/dev/null');
         $this->runRemote('cp {{ deploy_dir }}/.env.local {{ project_dir }} 2>/dev/null');
     }
 
@@ -46,7 +47,7 @@ return new class extends DefaultDeployer
     public function beforeFinishingDeploy()
     {
         $this->runRemote('cp {{ deploy_dir }}/repo/.env {{ project_dir }} 2>/dev/null');
-        $this->runRemote('cp {{ deploy_dir }}/repo/.env.dist {{ project_dir }} 2>/dev/null');
+        $this->runRemote('cp {{ deploy_dir }}/.env.local {{ project_dir }} 2>/dev/null');
         $this->log('Migrating database');
         $this->runRemote('{{ console_bin }} doctrine:migrations:migrate');
         $this->runLocal('say "The deployment has finished."');
