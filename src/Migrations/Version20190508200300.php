28c40fd426cf79a3679e9271f204e6c789efcdfe
= <bjoern.pfoster@gmail.com>
2019-05-08 22:03:17 +0200

    Replaced migrations with intial migration

--- src/Migrations/Version20190508200300.php
+++ src/Migrations/Version20190508200300.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190508152526 extends AbstractMigration
+final class Version20190508200300 extends AbstractMigration
 {
     public function getDescription() : string
     {
